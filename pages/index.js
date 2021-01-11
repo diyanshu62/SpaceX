1bb7452392097c8ac9c522c7f1fdf1a11ed43457
unknown <pandey.divyanshu@thinksys.com>
2021-01-11 08:30:51 +0530

    CC fixes

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,134 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from "react";
+import axios from 'axios';
+import Loader from "./components/Loader";
+import URL from './components/URL';
+import {
+  Heading,
+  TitleOnFilterComp,
+  FilterSectionContainerComp,
+  FilterHeading,
+  TileMainSectionComp,
+  MainWrapper,
+  MainContainer,
+  MainContainerFilter,
+} from "./components/GroupedComponents";
+import {
+  FilterValue
+} from './components/ComponentsWithProps';
+import Display from './components/TileDisplayContentComp';
 
-export default function Home() {
+/* Main component here */
+const Home = (prop) => {
+  const [data, setData] = useState(prop.data);
+  const [isFetching, setIsFetching] = useState(false);
+  const [filter, setFilter] = useState({"year":"","launch":"","landing":""});
+
+  function handleFilter(param, type) {
+    var url = "";
+    if(type == "year" && param == ""){
+      url = URL;
+      setFilter({...filter})
+    } else if(type == "year" && param != ""){
+      url = URL +"?launch_year="+param;
+      setFilter({"year":param,...filter});
+    } else{}
+
+    if(type == "launch" && param ==  ""){
+      url = URL;
+      setFilter({...filter})
+    } else if(type == "launch" && (param === "True" || param === "False")){
+      url = URL+"?launch_success=" +param.toLowerCase();
+      setFilter({"launch":param,...filter});
+    } else{}
+
+    if(type == "landing" && param ==  ""){
+      url = URL;
+      setFilter({...filter})
+    } else if(type == "landing" && (param === "True" || param === "False"))  {
+      url = URL+"?land_success=" +param.toLowerCase();
+      setFilter({"landing":param,...filter});
+    } else{}
+    
+    setIsFetching(true);
+    axios.get(url)
+      .then(res => {
+        setData(res.data);
+        setIsFetching(false);
+      })
+      .catch((err)=>{
+        throw(err);
+      });
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MainWrapper>
+      {isFetching ? <Loader /> : ""}
+      <Heading />
+      <MainContainer>
+        <MainContainerFilter>
+          <FilterHeading title="Filters" />
+          <FilterSectionContainerComp>
+            <TitleOnFilterComp text="Launch Year" />
+            <div className="sp-filter-option">
+              <FilterValue year="2006" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2007" type="year" handleFilterForYear={handleFilter} />
+            </div>
+            <div className="sp-filter-option">
+              <FilterValue year="2008" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2009" type="year" handleFilterForYear={handleFilter} />
+            </div>
+            <div className="sp-filter-option">
+              <FilterValue year="2010" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2011" type="year" handleFilterForYear={handleFilter} />
+            </div>
+            <div className="sp-filter-option">
+              <FilterValue year="2012" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2013" type="year" handleFilterForYear={handleFilter} />
+            </div>
+            <div className="sp-filter-option">
+              <FilterValue year="2014" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2015" type="year" handleFilterForYear={handleFilter} />
+            </div>
+            <div className="sp-filter-option">
+              <FilterValue year="2016" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2017" type="year" handleFilterForYear={handleFilter} />
+            </div>
+            <div className="sp-filter-option">
+              <FilterValue year="2018" type="year" handleFilterForYear={handleFilter} />
+              <FilterValue year="2019" type="year" handleFilterForYear={handleFilter} />
+            </div>
+          </FilterSectionContainerComp>
+          <FilterSectionContainerComp>
+            <TitleOnFilterComp text="Successful Launch" />
+            <div className="sp-filter-option">
+              <FilterValue year="True" type="launch"  handleFilterForYear={handleFilter} />
+              <FilterValue year="False" type="launch" handleFilterForYear={handleFilter} />
+            </div>
+          </FilterSectionContainerComp>
+          <FilterSectionContainerComp>
+            <TitleOnFilterComp text="Successful Landing" />
+            <div className="sp-filter-option">
+              <FilterValue year="True" type="landing" handleFilterForYear={handleFilter} />
+              <FilterValue year="False" type="landing" handleFilterForYear={handleFilter} />
+            </div>
+          </FilterSectionContainerComp>
+        </MainContainerFilter>
+        <TileMainSectionComp>
+          {data.length > 0 ? data.map(Display) : "No records"}
+        </TileMainSectionComp>
+      </MainContainer>
+    </MainWrapper>
+  );
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getStaticProps(context) {
+  const res = await fetch(
+    URL
+  );
+  const data = await res.json();
+  return {
+    props: { data: data },
+  };
 }
+
+export default Home;
